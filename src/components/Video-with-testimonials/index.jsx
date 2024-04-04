72827982634c501c1de3b71552847dd640950909
mardaniyan <p.mardaniyan@gmail.com>
2024-04-04 11:06:47 +0330

    first commit

---
+++ src/components/Video-with-testimonials/index.jsx
@@ -0,0 +1,157 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ModalVideo from "react-modal-video";
+import "react-modal-video/css/modal-video.css";
+
+const VideoWithTestimonials = () => {
+  const [isOpen, setOpen] = React.useState(false);
+  React.useEffect(() => {
+    console.clear();
+  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <section className="block-sec">
+      <div
+        className="background bg-img section-padding pb-0"
+        style={{ backgroundImage: `url(/img/slid/1.jpg)` }}
+        data-overlay-dark="8"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="vid-area">
+                <div className="vid-icon">
+                  {typeof window !== "undefined" && (
+                    <ModalVideo
+                      channel="vimeo"
+                      autoplay
+                      isOpen={isOpen}
+                      videoId="127203262"
+                      onClose={() => setOpen(false)}
+                    />
+                  )}
+                  <a
+                    className="vid"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setOpen(true);
+                    }}
+                    href="https://vimeo.com/127203262"
+                  >
+                    <div className="vid-butn">
+                      <span className="icon">
+                        <i className="fas fa-play"></i>
+                      </span>
+                    </div>
+                  </a>
+                </div>
+
+                <div className="cont">
+                  <h3 className="wow" data-splitting>
+                    So that&apos;s us. There&apos;s no other way to put it.
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-5 offset-lg-1">
+              <div className="testim-box">
+                <div className="head-box">
+                  <h6 className="wow fadeIn" data-wow-delay=".5s">
+                    Our Clients
+                  </h6>
+                  <h4 className="wow fadeInLeft" data-wow-delay=".5s">
+                    What Client&apos;s Say?
+                  </h4>
+                </div>
+                <Slider
+                  {...settings}
+                  className="slic-item wow fadeInUp"
+                  data-wow-delay=".5s"
+                >
+                  <div className="item">
+                    <p>
+                     Although I have a degree in physical education, I still participated in these courses and I am satisfied
+                    </p>
+                    <div className="info">
+                      <div className="img">
+                        <div className="img-box">
+                          <img src="/img/clients/1.jpg" alt="" />
+                        </div>
+                      </div>
+                      <div className="cont">
+                        <div className="author">
+                          <h6 className="author-name custom-font">
+                            Alex Regelman
+                          </h6>
+                          <span className="author-details">
+                            Tennis player, Colabrio
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <p>
+                     It really has a great grass field. I suggest this academy to register
+                    </p>
+                    <div className="info">
+                      <div className="img">
+                        <div className="img-box">
+                          <img src="/img/clients/2.jpg" alt="" />
+                        </div>
+                      </div>
+                      <div className="cont">
+                        <div className="author">
+                          <h6 className="author-name custom-font">
+                            Patri AlGodin
+                          </h6>
+                          <span className="author-details">
+                           football player, Colabrio
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <p>
+                      I participated in many international tours of the university and won international tournaments
+                    </p>
+                    <div className="info">
+                      <div className="img">
+                        <div className="img-box">
+                          <img src="/img/clients/3.jpg" alt="" />
+                        </div>
+                      </div>
+                      <div className="cont">
+                        <div className="author">
+                          <h6 className="author-name custom-font">
+                            Gonzalo Sereg
+                          </h6>
+                          <span className="author-details">
+                            cyclist, Colabrio
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Slider>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default VideoWithTestimonials;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import DarkTheme from '../layouts/Dark'
+
+import Homepage1 from './home/home1-dark';
+
+export default function Home() {
+  return (
+    <DarkTheme>
+      <Homepage1 />
+    </DarkTheme>
+  )
+}
